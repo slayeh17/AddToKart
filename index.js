7738cbc4384fed7945a41ceaf2195068713a7da1
Debak Roy <debakroy7@gmail.com>
2023-04-14 16:09:04 +0530

    third commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const item = document.getElementById("item-name");
-const addBtn = document.getElementById("AddToKart");
+const addBtn = document.getElementById("add-to-cart");
 const list = document.querySelector(".items");
 
 const appSettings = {
@@ -14,8 +14,49 @@ const database = getDatabase(app);
 const itemsListInDB = ref(database, "items-list");
 
 addBtn.addEventListener('click', ()=> {
-    let itemName = item.value;
-    item.value = "";
-    list.innerHTML += `<li class="item">${itemName}</li>`
-    // push(itemsListInDB, itemName);
+    if(item.value != "") {
+        let itemName = item.value;
+        push(itemsListInDB, itemName);
+        clearInputField();
+    }
+    // addItem(itemName);
 });
+
+onValue(itemsListInDB, function(snapshot) {
+    if(snapshot.exists()==true) {
+        let itemArray = Object.entries(snapshot.val());
+        clearItemList();
+        for(let i=0; i<itemArray.length; i++) {
+        let currentItem = itemArray[i];
+        let currentItemID = currentItem[0];
+        let currentItemValue = currentItem[1];
+        addItem(currentItem);
+        }
+    } 
+    else {
+        list.innerHTML = "<p style='color: #492a04; font-size: 30px;'>No items here... yet</p>"
+    }
+});
+
+function clearItemList() {
+    list.innerHTML = "";
+}
+
+function clearInputField() {
+    item.value = "";
+}
+
+function addItem(item) {
+    let li = document.createElement("li");
+    li.classList.add("item");
+    let itemID = item[0];
+    let itemValue = item[1];
+    li.innerText = itemValue;
+    list.append(li);
+
+
+    li.addEventListener("dblclick", ()=> {
+        let exactLocationOfItemInDB = ref(database, `items-list/${itemID}`);
+        remove(exactLocationOfItemInDB);
+    })
+}
